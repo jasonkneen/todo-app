d19bd70c67eb5843e14fe0c4299f9af5285d7497
jasonkneen <jason@bouncingfish.com>
2017-03-18 23:31:04 +0000

    remove reste config

--- app/alloy.js
+++ app/alloy.js
@@ -12,76 +12,14 @@
 
 Alloy.Globals.reste = require("reste")();
 
+Alloy.Globals.reste.createCollection("tasks", [{
+    name: "Do some stuff",
+    completed: false
+}, {
+    name: "Shopping",
+    completed: true
+}]);
 
-
-
-//Alloy.Globals.reste.createCollection("tasks", [{
-//    name: "Do some stuff",
-//    completed: false
-//}, {
-//    name: "Shopping",
-//    completed: true
-//}]);
-
-//Alloy.Collections.tasks.on("change", function() {
-//    Ti.App.Properties.setObject("tasks", Alloy.Collections.tasks.toJSON());
-//});
-
-function getSessionToken() {
-    return Alloy.Globals.user.sessionToken;
-}
-
-Alloy.Globals.reste.config({
-    timeout: 10000,
-    debug: true,
-    autoValidateParams: false,
-    url: "https://api.parse.com/1/",
-    requestHeaders: {
-        "X-Parse-Application-Id": "51kkhOJfC6VZsPBAnUzsPVWXrSpruneYwTRdvTPV",
-        "X-Parse-REST-API-Key": "coIVSdAzHHy60mBZScPOtr04kOMozrEtCKUmCagJ",
-        "Content-Type": "application/json",
-        //"X-Parse-Session-Token": getSessionToken()
-    },
-    models: [{
-        name: "task",
-        id: "objectId",
-        update: "updateTask",
-        collections: [{
-            name: "tasks",
-            content: "results",
-            read: "getTasks"
-        }]
-    }],
-    methods: [{
-        name: "getTasks",
-        get: "classes/todo"
-    }, {
-        name: "updateTask",
-        put: "classes/todo/<objectId>",
-    }],
-    onLoad: function(e, callback) {
-        callback(e);
-    },
-    onError: function(e, retry) {
-
-        console.log(e);
-
-        if (!e.code) {
-            var dialog = Ti.UI.createAlertDialog({
-                title: "Connection problem",
-                message: "There was a network error -- check and retry.",
-                buttonNames: ["Cancel", "Retry"]
-            });
-
-            dialog.addEventListener("click", function(e) {
-                if (e.index == 1) {
-                    retry();
-                } else {
-                    return;
-                }
-            });
-
-            dialog.show();
-        }
-    }
+Alloy.Collections.tasks.on("change", function() {
+    Ti.App.Properties.setObject("tasks", Alloy.Collections.tasks.toJSON());
 });
